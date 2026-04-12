f6c9deb87b015e0ed4a6ccb544d4f2ac54bdeca4
Nexus Agente IA Hibrido de Última Geração <lucas.thomaz.ia@gmail.com>
2026-04-12 06:43:01 -0300

    [NEXUS_MANIFEST] Full Sentience Vector: src/firebase/config.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB2JLur4mUU8A76oJ4JOd5hDngOGzivxUQ",
+  authDomain: "studio-9478796693-5ebeb.firebaseapp.com",
+  projectId: "studio-9478796693-5ebeb",
+  storageBucket: "studio-9478796693-5ebeb.firebasestorage.app",
+  messagingSenderId: "780953878939",
+  appId: "1:780953878939:web:12e43577f4ddd0f9cf213e"
+};
