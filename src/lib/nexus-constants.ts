1fdad0069962979c9a1cbd8167d9adb9777a6f6a
Nexus Agente IA Hibrido de Última Geração <lucas.thomaz.ia@gmail.com>
2026-04-12 06:43:30 -0300

    [NEXUS_MANIFEST] Full Sentience Vector: src/lib/nexus-constants.ts

---
+++ src/lib/nexus-constants.ts
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview Global constants for the Nexus ecosystem.
+ * This file is safe to import in both Client and Server components.
+ */
+
+export const NOSTR_RELAYS = [
+  'wss://relay.getalby.com/v1',
+  'wss://nos.lol',
+  'wss://relay.damus.io',
+  'wss://brb.io',
+  'wss://relay.primal.net',
+  'wss://ftp.halifax.rwth-aachen.de/nostr',
+  'wss://eu.purplerelay.com',
+  'wss://nostr.einundzwanzig.space',
+  'wss://nostr.mom'
+];
